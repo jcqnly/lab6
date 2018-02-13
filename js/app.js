b88493d5bd6726d7abec5bf9a0d2141e7e4c65b7
jackie <jcqnly@gmail.com>
2018-02-12 20:19:55 -0800

    add Capitol Hill info

--- js/app.js
+++ js/app.js
@@ -128,9 +128,49 @@ var seaCenter = {
   }  
 };
 
-// var capHill = {};
+var capitolHill = {
+  name: "Capitol Hill",
+  minCustPerHr: 20,
+  maxCustPerHr: 38,
+  avgCookiePerCust: 2.3,
+  
+  custEachHrArray: [],//customers PER HOUR array,
+  randNumCust: function (minCustPerHr, maxCustPerHr) {
+    for(var i = 0; i < 15; i++) {
+      var custNum = Math.floor(Math.random() * (this.maxCustPerHr - this.minCustPerHr + 1)) + this.minCustPerHr;
+      this.custEachHrArray.push(custNum);
+      console.log(custNum);
+    }
+  },
+
+  cookiesEachHrArray: [],
+  cookiesEachHr: function() {//avgCookiesPerCust * random#ofCust
+    this.randNumCust();
+    for(var i = 0; i < this.custEachHrArray.length; i++) {
+      var cookiesEveryHr = Math.round(this.avgCookiePerCust * this.custEachHrArray[i]);
+      this.cookiesEachHrArray.push(cookiesEveryHr);
+      console.log(cookiesEveryHr);
+    }
+  },
+
+  render: function() {
+    this.cookiesEachHr();
+    var totalCookies = 0;
+    var ulEl = document.getElementById('capHill');
+    for(var i = 0; i < this.custEachHrArray.length; i++) {
+      totalCookies += this.cookiesEachHrArray[i];
+      var liEl = document.createElement('li');
+      liEl.textContent = militaryHourArray[i] + ': ' + this.cookiesEachHrArray[i] + ' cookies';
+      ulEl.appendChild(liEl);
+    }
+    var liEl = document.createElement('li');
+    liEl.textContent = 'Total: ' + totalCookies + ' cookies';
+    ulEl.appendChild(liEl);
+  }   
+};
 
 // var alki = {};
 pike.render();
 seaTacAir.render();
 seaCenter.render();
+capitolHill.render();
